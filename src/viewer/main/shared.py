a0b19e3943ad63068aa620e6243edf95c579954c
Alex Sandro <geniofuturo@gmail.com>
2012-09-09 21:01:15 -0300

    renomeando função formatadora de data para 'getFormatedDate'.

--- src/viewer/main/shared.py
+++ src/viewer/main/shared.py
@@ -6,7 +6,7 @@ from django.utils.dates import MONTHS
 from dateutil import parser
 import admin
 
-def getDateISOFormat(dateString):
+def getFormatedDate(dateString):
     p = parser.parse(dateString)
     return p.strftime("%d/%m/%Y às %H:%M:%S")
 
@@ -60,22 +60,23 @@ class Statistic(object):
     def __init__(self, project_id, year=0, month=0, **params):
         self.params = params
         self.project_id = project_id
-        self.date = self.getDateForYearMonth(year, month)
         
         self.project = project = Project.objects.get(key=project_id)
         try: self.yearlyplan = project.yearlyplan_set.get(created_at__year=year)
         except: self.yearlyplan = None
         
+        self.date = self.getDateForYearMonth(year, month)
+        
         # guarda as horas consumidas de todos o meses
         self.yearlySpent = 0.0
         
     def update(self, year, month):
-        self.date = self.getDateForYearMonth(year, month)
+        self.date = date = self.getDateForYearMonth(year, month)
         try:
             # procura o plano mensal do dentro do plano anual
             monthlyPlan = self.yearlyplan.monthlyplan_set.filter(
-                starts__lte = self.date.isoformat(), 
-                finished__gt = self.date.isoformat()
+                starts__lte = date.isoformat(), 
+                finished__gt = date.isoformat()
             )
             if monthlyPlan.count() > 0:
                 # Plano com periodo inicial, porém final ilimitado.
@@ -84,7 +85,7 @@ class Statistic(object):
             else:
                 # Plano mensal com témino ilimitado.
                 monthlyPlan = self.yearlyplan.monthlyplan_set.filter(
-                    starts__lte = self.date.isoformat(), finished = None
+                    starts__lte = date.isoformat(), finished = None
                 )
             try:
                 monthlyPlan = monthlyPlan[monthlyPlan.count()-1]
@@ -110,7 +111,7 @@ class Statistic(object):
             "estimated": 0.0, "spent": 0.0,
             "project": self.project.name,
             "yearly_hours": self.yearly_hours,
-            "created": self.date.strftime("%m/%Y"),
+            "created": date.strftime("%m/%Y"),
             "total_hours": total_hours,
             "plan_hours": plan_hours,
             "hours_add":  hours_add,
@@ -141,20 +142,22 @@ class Statistic(object):
     @staticmethod
     def getDateForYearMonth(year, month):
         now = datetime.datetime.now()
+        
         if not month: month = now.month
         if not year: year = now.year
+        
         maxdays = calendar.monthrange(year, month)[-1]
         d = datetime.datetime(year, month, maxdays)
         return d.date()
-
-    def get_date(self):
-        return self.date
-
+        
     def __getitem__(self, key):
         return self.statistic[key]
 
     def __setitem__(self, key, value):
         self.statistic[key] = value
-
+        
+    def get_date(self):
+        return self.date
+    
     def get_data(self):
         return self.statistic
